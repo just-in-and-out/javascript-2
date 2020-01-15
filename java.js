cac59af1406e9fc2a58c1fd9e68f214878eab49e
just-in-and-out <54847960+just-in-and-out@users.noreply.github.com>
2020-01-15 09:56:35 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,5 @@
+document.querySelector("#wrapper").style.backgroundColor = "red";
+document.getElementById("paginatitel").innerText = "Dit is de paginatitel";
+document.getElementById("subkop").innerText = "Dit is de subkop";
+document.getElementById("aap1").src = "Captain_Qwark.png";
+document.getElementById("aap2").src = "download.jpg";
